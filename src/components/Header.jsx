bf28a6e021aa65cef69ac26c7a876fe7ca569539
SwapnilV26 <swapnilvish2812@gmail.com>
2023-10-19 14:56:56 +0530

    Added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="fixed z-10 bg-gray-200 w-full transition duration-300 ease-in-out">
       <div className="mx-24 flex items-center justify-between h-16 md:h-20">
-        <div>LOGO</div>
+        <div>
+          <img className="w-40" src="./algocanvas.png" alt="" srcset="" />
+        </div>
         <nav>
           <ul className="flex grow justify-end flex-wrap items-center gap-5 text-lg">
             <li>
